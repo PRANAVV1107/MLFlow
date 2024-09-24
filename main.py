831c0a5b7dbac9a58f32239d4cb536c45d714d20
PranavV110 <vishwanath.p@northeastern.edu>
2024-09-24 19:15:05 -0400

    Data Ingestion Pipeline Push

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from MLFlow import logger
+from MLFlow.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
-logger.info("welcome to custom log")
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
+    obj = DataIngestionTrainingPipeline()
+    obj.main()
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\n x==============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
