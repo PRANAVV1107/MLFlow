1312e8475cc423291a608644daedf58f0f4dbf20
PranavV110 <vishwanath.p@northeastern.edu>
2024-09-25 19:16:53 -0400

    Added data_validation

--- src/MLFlow/config/configuration.py
+++ src/MLFlow/config/configuration.py
@@ -1,6 +1,7 @@
 from MLFlow.constants import *
 from MLFlow.utils.common import read_yaml, create_directories
 from MLFlow.entity.config_entity import DataIngestionConfig
+from MLFlow.entity.config_entity import DataValidationConfig
 
 #configuration manager - reading all yaml files
 class ConfigurationManager:
@@ -29,3 +30,33 @@ class ConfigurationManager:
         )    
 
         return data_ingestion_config 
+    
+
+    def __init__(
+        self,
+        config_filepath = CONFIG_FILE_PATH,
+        params_filepath = PARAMS_FILE_PATH,
+        schema_filepath = SCHEMA_FILE_PATH):
+
+        self.config = read_yaml(config_filepath)
+        self.params = read_yaml(params_filepath)
+        self.schema = read_yaml(schema_filepath)
+
+        create_directories([self.config.artifacts_root])
+
+
+    
+    def get_data_validation_config(self) -> DataValidationConfig:
+        config = self.config.data_validation
+        schema = self.schema.COLUMNS
+
+        create_directories([config.root_dir])
+
+        data_validation_config = DataValidationConfig(
+            root_dir=config.root_dir,
+            STATUS_FILE=config.STATUS_FILE,
+            unzip_data_dir = config.unzip_data_dir,
+            all_schema=schema,
+        )
+
+        return data_validation_config
--- src/MLFlow/pipeline/stage_01_data_ingestion.py
+++ src/MLFlow/pipeline/stage_01_data_ingestion.py
@@ -1,4 +1,4 @@
-from MLFlow.config.configuration import ConfigurationManager
+from MLFlow.config.configuration import *
 from MLFlow.components.data_ingestion import DataIngestion
 from MLFlow import logger
 
@@ -9,11 +9,14 @@ class DataIngestionTrainingPipeline:
         pass
 
     def main(self):
-        config = ConfigurationManager()
-        data_ingestion_config = config.get_data_ingestion_config()
-        data_ingestion = DataIngestion(config=data_ingestion_config)
-        data_ingestion.download_file()
-        data_ingestion.extract_zip_file()
+        try:
+            config = ConfigurationManager()
+            data_ingestion_config = config.get_data_ingestion_config()
+            data_ingestion = DataIngestion(config=data_ingestion_config)
+            data_ingestion.download_file()
+            data_ingestion.extract_zip_file()
+        except Exception as e:
+            raise e
 
 if __name__ == '__main__':
     try:
