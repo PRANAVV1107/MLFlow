9f3189e73819af82e8daa6ddf91011986ba31b9e
PranavV110 <vishwanath.p@northeastern.edu>
2024-09-28 11:16:19 -0400

    Model Trainer Added

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from MLFlow import logger
 from MLFlow.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from MLFlow.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-
+from MLFlow.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from MLFlow.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
 STAGE_NAME = "Data Ingestion stage"
 try:
     logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
@@ -18,7 +19,28 @@ try:
     logger.info(f">>>>>>>>>> stage {STAGE_NAME} started <<<<<<<<<<")
     obj = DataValidationTrainingPipeline()
     obj.main()
-    logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\n x==============x")
+    logger.info(f">>>> stage {STAGE_NAME} completed <<<<\n\nx==============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+
+STAGE_NAME = "Data Transformation Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_ingestion = DataTransformationTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx==============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Model Trainer Stage"
+try:
+    logger.info(f">>>>> stage {STAGE_NAME} started <<<<<")
+    data_ingestion = ModelTrainerTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>> stage {STAGE_NAME} completed <<<<<\n\nx=============x")
 except Exception as e:
     logger.exception(e)
     raise e
--- src/MLFlow/config/configuration.py
+++ src/MLFlow/config/configuration.py
@@ -1,7 +1,6 @@
 from MLFlow.constants import *
 from MLFlow.utils.common import read_yaml, create_directories
-from MLFlow.entity.config_entity import DataIngestionConfig
-from MLFlow.entity.config_entity import DataValidationConfig
+from MLFlow.entity.config_entity import (DataIngestionConfig, DataValidationConfig, DataTransformationConfig, ModelTrainerConfig)
 
 #configuration manager - reading all yaml files
 class ConfigurationManager:
@@ -32,18 +31,6 @@ class ConfigurationManager:
         return data_ingestion_config 
     
 
-    def __init__(
-        self,
-        config_filepath = CONFIG_FILE_PATH,
-        params_filepath = PARAMS_FILE_PATH,
-        schema_filepath = SCHEMA_FILE_PATH):
-
-        self.config = read_yaml(config_filepath)
-        self.params = read_yaml(params_filepath)
-        self.schema = read_yaml(schema_filepath)
-
-        create_directories([self.config.artifacts_root])
-
 
     
     def get_data_validation_config(self) -> DataValidationConfig:
@@ -60,3 +47,36 @@ class ConfigurationManager:
         )
 
         return data_validation_config
+    
+    
+   
+    def get_data_transformation_config(self) -> DataTransformationConfig:
+        config = self.config.data_transformation
+
+        create_directories([config.root_dir])
+
+        data_transformation_config = DataTransformationConfig(
+            root_dir = config.root_dir,
+            data_path = config.data_path
+        )    
+
+        return data_transformation_config  
+    
+    def get_model_trainer_config(self) -> ModelTrainerConfig:
+        config = self.config.model_trainer
+        params = self.params.ElasticNet
+        schema = self.schema.TARGET_COLUMN
+
+        create_directories([config.root_dir])
+
+        model_trainer_config = ModelTrainerConfig(
+            root_dir = config.root_dir,
+            train_data_path = config.train_data_path,
+            test_data_path = config.test_data_path,
+            model_name = config.model_name,
+            alpha = params.alpha,
+            l1_ratio = params.l1_ratio,
+            target_column = schema.name
+        )
+
+        return model_trainer_config
